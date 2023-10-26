5cd0b8e2b67d12abec445fbd7826e8a2a3b7472c
SyrianTiger74 <maher.abou.asla@gmail.com>
2023-10-27 01:57:57 +0300

    configuration 1_paskaita

---
+++ 1_paskaita/src/App.jsx
@@ -0,0 +1,14 @@
+function App () {
+  return (<main>
+    <header>
+      <h1>Palestine will be free</h1>
+    </header>
+    <section>
+      <p>resistance axis</p>
+    </section>
+      </main>)
+    }
+    
+    export default App 
+
+  
