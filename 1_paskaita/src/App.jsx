8fbc7102153f0220a4409295fccf0a8ff4b029d9
SyrianTiger74 <maher.abou.asla@gmail.com>
2023-12-09 06:32:01 +0200

    dont know what is wrong

--- 1_paskaita/src/App.jsx
+++ 1_paskaita/src/App.jsx
@@ -1,14 +1,14 @@
 function App () {
-  return (<main>
-    <header>
-      <h1>Palestine will be free</h1>
-    </header>
-    <section>
-      <p>resistance axis</p>
-    </section>
-      </main>)
-    }
-    
-    export default App 
 
-  
+
+  return (<main>
+<header>
+  <h1>Palestine will be free</h1>
+</header>
+<section>
+  <p>resistance axis</p>
+</section>
+  </main>)
+}
+
+import default App
